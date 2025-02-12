de168d723d2a23bf8d86033da83db8fff784550c
48pz <huanmiaoi@outlook.com>
2025-02-12 17:41:11 +1000

    aggregating rest api and using RESTClient instead of feign

--- order-service/src/main/java/com/flow/microservices/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/flow/microservices/order_service/OrderServiceApplication.java
@@ -2,13 +2,9 @@ package com.flow.microservices.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
-@EnableFeignClients
 public class OrderServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
